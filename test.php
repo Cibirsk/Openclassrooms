065a82037f453ab04a4d3bdce5fe5fc9f7bc06a6
Cibirsk <stlv@gmx.com>
2021-11-19 17:52:09 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,4 +4,16 @@ $yourFirstName=$_POST['firstname'];
 $yourName=$_POST['name'];
 $result=$_POST['result'];
 
+switch($result){
+    case 0:
+        echo "pas terrible";
+    break;
+
+    case 20:
+        echo " $yourFirstName tu es magnifique";
+    break;
+
+    default:
+        echo "pas de note cher $yourName";
+}
 ?>
