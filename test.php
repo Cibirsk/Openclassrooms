b8b4af0c081f61d4a214960a6576c4891818b0f8
Cibirsk <stlv@gmx.com>
2021-11-19 10:44:24 +0100

    none

--- test.php
+++ test.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <?php
+<?php
 
-    $myVar="non";
-    if($myVar=="non"){
-        echo "la variable étant $myVar c'est refusé";
-    }
-    elseif($myVar=="oui"){
-        echo "la réponse étant $myVar vous pouvez entrer";
-    }
-    else{
-        echo "il me faut une réponse";
-    }
+$yourFirstName=$_POST['firstname'];
+$yourName=$_POST['name'];
+$result=$_POST['result'];
 
-   ?>
-</body>
-</html>
+?>
