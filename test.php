52813043b8a3c8bb3a44ce4975146c2f575bc9de
Cibirsk <stlv@gmx.com>
2021-11-20 11:07:46 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,32 @@
 <?php
 
-    $recette1=array(
+    $recette1=[
+    [
         "auteur" => "Bob",
         "nom" => "cassoulet",
-        "temps de preparation" => "2 heures",
-        "niveau d'expérience" => "avancée",
+        "temps de préparation" => "2 heures",
+        "niveau_xp" => "avancée",
         "ingrédients" => "haricots, lard, canard"
-    );
+    ],
+    [
+        "auteur" => "Mike",
+        "nom" => "salade",
+        "temps de préparation" => "10 minutes",
+        "niveau_xp" => "simple",
+        "ingrédients" => "salade, tomates"
+    ],
+    [
+        "auteur" => "Dan",
+        "nom" => "couscous",
+        "temps de préparation" => "1 heure",
+        "niveau_xp" => "moyen",
+        "ingrédients" => "semoule, raison, menthe"
+    ]
+    ];
 
 
-    foreach($recette1 as $clef => $value){
-        echo "$clef = $value <br>";
+    foreach($recette1 as $value){
+        echo "C'est $value[auteur] qui a conçu $value[nom] <br>";
+        echo "Cela nécessite un niveau $value[niveau_xp]<br>";
     }
 ?>
