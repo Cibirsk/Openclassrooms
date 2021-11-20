fb48acc9bbb53d573fae3b998586bf3df09b21e3
Cibirsk <stlv@gmx.com>
2021-11-20 14:07:12 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,21 +4,21 @@
     [
         "auteur" => "Bob",
         "nom" => "cassoulet",
-        "temps de préparation" => "2 heures",
+        "temps_prep" => "2 heures",
         "niveau_xp" => "avancée",
         "ingrédients" => "haricots, lard, canard"
     ],
     [
         "auteur" => "Mike",
         "nom" => "salade",
-        "temps de préparation" => "10 minutes",
+        "temps_prep" => "10 minutes",
         "niveau_xp" => "simple",
         "ingrédients" => "salade, tomates"
     ],
     [
         "auteur" => "Dan",
         "nom" => "couscous",
-        "temps de préparation" => "1 heure",
+        "temps_prep" => "1 heure",
         "niveau_xp" => "moyen",
         "ingrédients" => "semoule, raison, menthe"
     ]
@@ -27,6 +27,6 @@
 
     foreach($recette1 as $value){
         echo "C'est $value[auteur] qui a conçu $value[nom] <br>";
-        echo "Cela nécessite un niveau $value[niveau_xp]<br>";
+        echo "Cela nécessite un niveau $value[niveau_xp] et vous prendra $value[temps_prep]<br>";
     }
 ?>
