acfffc7411ee1195aeefac2784dcb7f01124a29d
Cibirsk <stlv@gmx.com>
2021-11-21 09:45:07 +0100

    none

---
+++ formulaire.php
@@ -0,0 +1,10 @@
+<?php
+
+    $yourFirstName= $_POST['firstname'];
+    $yourName= $_POST['name'];
+    $yourTown= $_POST['town'];
+    echo 'your firstname is: ' . $yourFirstName . '<br />';
+    echo 'your name is: ' . $yourName . '<br />';
+    echo 'you live in: ' . $yourTown;
+
+?>
